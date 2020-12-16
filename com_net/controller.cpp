91b0d0b42ad0058a1535544403f1fe2d0f8bc052
zhzhz <932496816@qq.com>
2020-12-16 09:32:46 +0800

    修复关闭多个gui出错的bug

--- com_net/controller.cpp
+++ com_net/controller.cpp
@@ -55,7 +55,7 @@ Controller::~Controller()
 //发送采集数据
 void Controller::get_data(data_exchange data, QTcpSocket *tcp)
 {
-    //qDebug() << "2.gui发送读请求给Controller::get_data";
+    qDebug() << "2.gui发送读请求给Controller::get_data";
     emit operate(data, tcp);
 }
 
